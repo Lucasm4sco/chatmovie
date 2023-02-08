da99976510d5af1f27e5111594569e0e8847bcb7
Lucasm4sco <lmbp202@gmail.com>
2023-02-08 20:39:24 -0300

    feat: route structures

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import router from "./routes/Routes.js";
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+app.use(router);
